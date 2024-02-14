c6733adb7304702b3c21dc2ffa11ad8f2f5ce81e
rayquekCW <ray.quek@gmail.com>
2024-02-14 21:26:54 +0800

    feat: link home page to questions view

--- BadUI/src/views/HomePage.vue
+++ BadUI/src/views/HomePage.vue
@@ -3,53 +3,60 @@
     <div class="intro">
       <h1>HUNTING FOR BAD UI</h1>
     </div>
-    <main>
+    <main style="text-align: center">
       <h3>First all all, some information!</h3>
       <NamePicker></NamePicker>
 
       <h2>Phone Number:</h2>
       <div class="phoneNumberCont flex flex-column align-items-center justify-content-center">
         <span>{{ phoneNumber }}</span>
-        <Slider v-model="phoneNumber" :min="0" :max="9999999999" class="w-30rem" ></Slider>
+        <Slider v-model="phoneNumber" :min="0" :max="9999999999" class="w-30rem"></Slider>
       </div>
       <h2>Age</h2>
       <div class="ageCont flex flex-column align-items-center justify-content-center">
-        <p>{{age}}</p>
-        <Slider v-model="age" class="w-30rem" :min="0" :max="200"/>
+        <p>{{ age }}</p>
+        <Slider v-model="age" class="w-30rem" :min="0" :max="200" />
       </div>
-
-
-
+      <button class="submit" @click="handleSubmit">Submit</button>
     </main>
   </div>
 </template>
 
 <script>
 import NamePicker from '@/components/NamePicker.vue'
-import Slider from 'primevue/slider';
-
+import Slider from 'primevue/slider'
 
 export default {
   components: {
     NamePicker,
-    Slider,
+    Slider
   },
   data() {
     return {
       phoneNumber: 0,
       age: 0
-    };
+    }
   },
+  methods: {
+    handleSubmit() {
+      this.$router.push('/questions')
+    }
+  }
 }
 </script>
 
 <style scoped>
 .mainContainer {
   display: flex;
-  flex-direction:column;
-  align-items:center;
-
+  flex-direction: column;
+  align-items: center;
 }
-
-
+.submit {
+  margin-top: 20px;
+  padding: 10px;
+  width: 50%;
+  background: rgb(78, 195, 129);
+  color: white;
+  border: none;
+}
 </style>
