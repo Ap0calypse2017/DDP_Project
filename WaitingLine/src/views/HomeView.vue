6a889804abcd4b83a61eb849d5c914dfb8efea80
Filippo Piloni <filippo.piloni@usi.ch>
2024-05-07 09:16:33 +0200

    Added OTP to enter specific queue

--- WaitingLine/src/views/HomeView.vue
+++ WaitingLine/src/views/HomeView.vue
@@ -1,17 +1,23 @@
 
 
 <template>
-  <main class="flex flex-column align-items-center justify-content-center w-full h-full">
+  <main class="flex flex-column align-items-center justify-content-center w-full h-full gap-4">
     <h1>TheAmazingSite&#8482</h1>
+    <div style="display:none">
+      <h2>Enter Queue Code</h2>
+      <InputOTP></InputOTP>
+    </div>
     <Button label="Join queue" @click="this.$router.push('/waiting')"></Button>
   </main>
 </template>
 <script>
 import Button from 'primevue/button'
+import InputOTP from 'primevue/inputotp';
 
 export default {
   components: {
-    Button
+    Button,
+    InputOTP
   }
 }
 </script>
